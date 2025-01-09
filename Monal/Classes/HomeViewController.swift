68994a30e01d3e7f2be8eecd64c6b7e7655d6817
shweta11soni <io1912shweta@gmail.com>
2025-01-09 11:47:28 +0530

    Updated code in swift

---
+++ Monal/Classes/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  Monal
+//
+//  Created by shweta soni on 08/01/25.
+//  Copyright © 2025 monal-im.org. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
